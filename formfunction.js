e66cd6fe28b6451a1ae8fb9bee25809fba7da3d5
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2020-05-01 16:27:39 -0700

    Created feature 3 workflow

---
+++ formfunction.js
@@ -0,0 +1,11 @@
+function resetUserInput() {
+    document.getElementById("userInput").reset();
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
